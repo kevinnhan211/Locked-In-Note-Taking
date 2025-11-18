1f66c77794ee4e7a9f6f7928355243cba2f775d4
Kevin Nhan <kevinnhan.2.1.1@gmail.com>
2025-11-18 13:53:42 -0500

    Initial Commit

---
+++ LockedInNoteCreate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LockedInNoteCreate
+//
+//  Created by Kevin Nhan on 2025-11-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LockedInNoteCreate/LockedInNoteCreateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockedInNoteCreateApp.swift
+//  LockedInNoteCreate
+//
+//  Created by Kevin Nhan on 2025-11-18.
+//
+
+import SwiftUI
+
+@main
+struct LockedInNoteCreateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
